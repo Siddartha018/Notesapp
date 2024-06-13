8a06b00466c2a6048089dc5da774cbe92e343526
9640654679 <saisiddartha679@gmail.com>
2024-06-13 21:07:48 +0530

    update

---
+++ db/schema.ts
@@ -0,0 +1,28 @@
+
+
+import { relations } from "drizzle-orm";
+import { integer, text, boolean, pgTable,serial, time, timestamp } from "drizzle-orm/pg-core";
+
+export const users=pgTable("users",{
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  email:text("email").notNull(),
+  createdAt:timestamp("created_at").notNull().defaultNow(),
+  updatedAt:timestamp("upadated_at").notNull().defaultNow()
+})
+
+
+export const todos = pgTable("todos", {
+  id: integer("id").primaryKey(),
+  text: text("text").notNull(),
+  done: boolean("done").default(false).notNull(),
+  userId:integer("user_id").notNull().references(()=>users.id)
+});
+
+export const todosRelations=relations(todos,({one})=>({
+  user:one(users,{fields:[todos.userId],references:[users.id]}),
+}))
+
+export const usersRelations=relations(users,({many})=>({
+ todos:many(todos),
+}))
